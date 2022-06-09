f5a1a9a1283fa43416c60430f0cccd7a9d301caf
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2022-06-09 16:36:29 +0100

    added movie actors and recommenation

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,7 +1,236 @@
 import React from 'react';
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Rating,
+} from '@mui/material';
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { MovieList } from '..';
+import axios from 'axios';
+import {
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+} from '../../services/TMDB';
+
+import useStyles from './styles';
+import genreIcons from '../../assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const MovieInformation = () => {
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const {
+    data: recommendations,
+    isFetching: isRecommendationsFetching,
+    error: recommendationsError,
+  } = useGetRecommendationsQuery({
+    list: `/recommendations`,
+    movie_id: id,
+  });
+  const isMovieFavorited = false;
+  const isMovieWatchlisted = false;
+  const addToFavorites = () => {};
+  const addToWatchlist = () => {};
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  } else if (error) {
+    return (
+      <Box display="flex" justify="center" alignItems="center">
+        <Link to="/">Something has gone wrong, go back</Link>
+      </Box>
+    );
+  }
+  return (
+    <Grid container className={classes.containerSpaceRound}>
+      <Grid item sm={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h2" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceRound}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography
+              vatiant="subtitle1"
+              gutterBottom
+              styles={{ marginLeft: '10px' }}
+            >
+              {data.vote_average / 2} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime} minutes{' '}
+            {data?.spoken_languages.length > 0
+              ? ` / ${data?.spoken_languages[0].name}`
+              : ''}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre, i) => (
+            <Link
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+              key={genre.name}
+            >
+              <img
+                src={genreIcons[genre.name.toLowerCase()]}
+                className={classes.genreImage}
+                height={30}
+                alt={genre.name}
+              />
+              <Typography color="textPrimary" vatiant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
+        <Typography variant="h5" gutterBottom styles={{ marginTop: '10px' }}>
+          Overview
+        </Typography>
+        <Typography style={{ marginBottom: '2rem' }}>
+          {data?.overview}
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          Top Cast
+        </Typography>
+        <Grid item container spacing={2}>
+          {data &&
+            data?.credits?.cast
+              .map(
+                (character, i) =>
+                  character.profile_path && (
+                    <Grid
+                      key={i}
+                      item
+                      xs={4}
+                      md={2}
+                      component={Link}
+                      to={`/actors/${character.id}`}
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <img
+                        className={classes.castImage}
+                        src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`}
+                        alt={character.name}
+                      />
+                      <Typography color="textPrimary">
+                        {character?.name}
+                      </Typography>
+                      <Typography color="textSecondary">
+                        {character?.character.split('/')[0]}
+                      </Typography>
+                    </Grid>
+                  )
+              )
+              .splice(0, 6)}
+        </Grid>
+        <Grid item container styles={{ marginTop: '2rem' }}>
+          <div className={classes.buttonsContainer}>
+            <Grid item xs={12} sm={6} className={classes.ButtonsContainer}>
+              <ButtonGroup size="md" variant="outlined">
+                <Button
+                  target="_black"
+                  url="noopener noreferrer"
+                  href={data?.homepage}
+                  endIcon={<Language />}
+                >
+                  Website
+                </Button>
+                <Button
+                  target="_black"
+                  url="noopener noreferrer"
+                  href={`https://imdb.com/title/${data?.imdb_id}`}
+                  endIcon={<MovieIcon />}
+                >
+                  IMDB
+                </Button>
+                <Button onClick={() => {}} href="#" endIcon={<Theaters />}>
+                  Trailer
+                </Button>
+              </ButtonGroup>
+            </Grid>
+            <Grid item xs={12} sm={6} className={classes.ButtonsContainer}>
+              <ButtonGroup size="sm" variant="outlined">
+                <Button
+                  onClick={addToFavorites}
+                  endIcon={
+                    isMovieFavorited ? <FavoriteBorderOutlined /> : <Favorite />
+                  }
+                >
+                  {isMovieFavorited ? 'Unfavorited' : 'Favorite'}
+                </Button>
+                <Button
+                  onClick={addToWatchlist}
+                  endIcon={isMovieWatchlisted ? <Remove /> : <PlusOne />}
+                >
+                  Watchlist
+                </Button>
+                <Button
+                  endIcon={<ArrowBack />}
+                  sx={{ borderColor: 'primary.main' }}
+                >
+                  <Typography
+                    component={Link}
+                    to="/"
+                    color="inherit"
+                    variant="subtitle2"
+                    style={{ textDecoration: 'none' }}
+                  >
+                    Back
+                  </Typography>
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+      <Box marginTop="5rem" width="100%">
+        <Typography variant="h3" gutterBottom align="center">
+          You might also like
+        </Typography>
+        // Loop through recommended movies
+        {recommendations ? (
+          <MovieList movies={recommendations} numberOfMovies={12}></MovieList>
+        ) : (
+          <Box>Sorry, nothing found</Box>
+        )}
+      </Box>
+    </Grid>
+  );
 };
 
 export default MovieInformation;
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -32,10 +32,22 @@ export const tmdbApi = createApi({
         return `/movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+
+    // Get Movies
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
+    }),
+
+    // get user specific list
+    getRecommendations: builder.query({
+      query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
+    }),
   }),
 });
 
 export const {
   useGetMoviesQuery,
   useGetGenresQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
 } = tmdbApi;
