024b869a8f7765de14a904f9e0051f91ee43dbf2
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2022-06-17 08:33:12 +0100

    added featured movie card

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,28 +1,60 @@
 import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import { fetchToken } from '../utils';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from '../features/currentGenreOrCategory';
+import { fetchToken } from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
   const history = useHistory();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     alanBtn({
       key: '1f2c7fcdd1110b219baa2066d86b9ba42e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        // Taking alan props checking for the command
+        if (command === 'chooseGenre') {
+          // checking the genre searched is in the database
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+          console.log(foundGenre);
+          if (foundGenre) {
+            // if the genre is found, push to home and search genre
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            // if the found genre is not in the list (top rated, upcoming, popular categories)
+            const category = genreOrCategory.startsWith('top')
+              ? 'top_rated'
+              : genreOrCategory;
+
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
+          // checking for dar/light and settinng
           if (mode === 'light') {
             setMode('light');
           } else {
             setMode('dark');
           }
         } else if (command === 'login') {
+          // fetching token if command is login
           fetchToken();
         } else if (command === 'logout') {
+          // clears hisotyr and forwards to home if logout
           localStorage.clear();
-          window.location.href = '/';
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   CircularProgress,
   useMediaQuery,
-  Typeography,
+  Typography,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
-import { MovieList, Pagination } from '../index';
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { FeaturedMovie, MovieList, Pagination } from '../index';
 
 const Movies = () => {
   const [page, setPage] = useState(1);
@@ -22,7 +21,7 @@ const Movies = () => {
     searchQuery,
   });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -47,7 +46,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data?.results[0]} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination
         currentPage={page}
         setPage={setPage}
